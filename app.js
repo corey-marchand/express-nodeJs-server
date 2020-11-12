b0f75f4ec14f09a77ecd4f710b0462aff22453d9
corey-marchand <corey.marchand@me.com>
2020-11-11 20:17:20 -0800

    basic server routes

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-const validator = require('validator');
+const express = require('express');
 
+const app = express();
+
+app.get('', (req, res) => {
+    res.send('home');
+})
+
+app.get('/about', (req, res) => {
+    res.send('about');
+})
+
+app.get('/portfolio', (req, res) => {
+    res.send('portfoio');
+})
+
+app.get('/contact', (req, res) => {
+    res.send('contact');
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on Port 3000');
+})
